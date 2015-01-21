e2040679d5d47cf05a03c5594704fe18c140ce9e
Daniel Baird <daniel@danielbaird.com>
2015-01-21 17:28:31 +1000

    better map resizing

--- site/js/main.js
+++ site/js/main.js
@@ -1,29 +1,43 @@
 $(function(){
 
+    // init leaflet
+    var leafletmap = L.map('map', {
+        reuseTiles: true
+    });
+
+    function resizeMap(seconds) {
+        // repeatedly resize the map for however many seconds
+        var interval = 100; // milliseconds
+
+        // start resizing
+        var resizingId = setInterval(function() {
+            leafletmap.invalidateSize(true);
+        }, interval);
+
+        // stop resizing
+        setTimeout(function() {
+            clearInterval(resizingId);
+        }, seconds * 1000);
+    }
+
     // set the height of the contentwrapper to fill the screen.
     var $cw = $('.contentwrapper');
     var winHeight = $(window).height();
     $cw.height(winHeight - $cw.position().top);
+    resizeMap(1.5);
 
-    // init leaflet
-    var leafletmap = L.map('map', {
-    });
+
     leafletmap.setView([-13, 140], 4);
     // add an OpenStreetMap tile layer
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OSM</a> contributors'
     }).addTo(leafletmap);
 
-    function resizeMap() {
-        leafletmap.invalidateSize(true);
-    }
 
     // make a convenient testing button
     $('#mapon').click(function(event){
        $('.contentwrapper').toggleClass('map-active map-inactive');
-       for (var i = 1; i < 20; i++) {
-           setTimeout(resizeMap, i * 50);
-       }
+       resizeMap(1.5);
     });
 
 
